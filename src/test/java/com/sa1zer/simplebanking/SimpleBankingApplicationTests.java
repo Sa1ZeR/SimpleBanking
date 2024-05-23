8bd2c47716de0edf9afb2e1fb8ea4e12141cf83c
Serg1K <amsher2007@mail.ru>
2024-05-23 17:02:58 +1000

    init

---
+++ src/test/java/com/sa1zer/simplebanking/SimpleBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sa1zer.simplebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
